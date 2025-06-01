d3d47e4ef14e0beeea7dc153f75aeefac23bfcee
Daniel Nery <danielpontesnery@gmail.com>
2025-05-31 21:29:41 -0300

    feat: refactory.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 function getLoadingContext() {
